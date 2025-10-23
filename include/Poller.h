0755dc418e4ecb0c10eb2d1587d833cb926b5202
fenghen <fenghen.lj@antgroup.com>
2025-10-23 20:53:53 +0800

    更新抽象工厂接口Poller

--- include/Poller.h
+++ include/Poller.h
@@ -1,4 +1,4 @@
-#pragma once
+﻿#pragma once
 
 #include<vector>
 #include<unordered_map>
@@ -7,3 +7,25 @@
 
 class Channel;
 class EventLoop;
+
+class Poller
+{
+public:
+	using ChannelList = std::vector<Channel*>;
+	Poller(EventLoop* loop);
+	virtual ~Poller() = default;
+
+	//给所有IO复用保留统一接口
+	virtual Timestamp poll(int timeoutMs, ChannelList* activeChannels) = 0;
+	virtual void updateChannel(Channel* channel) = 0;
+	virtual void removeChannel(Channel* channel) = 0;
+
+	bool hasChannel(Channel* channel)const;
+
+	static Poller* newDefaultPoller(EventLoop* loop);
+protected:
+	using ChannelMap = std::unordered_map<int, Channel*>;
+	ChannelMap channels_;
+private:
+	EventLoop* ownerLoop_;
+};
